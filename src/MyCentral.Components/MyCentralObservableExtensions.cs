f97c199ca7cf4088c03d510e65a78e25fdf5e244
Taylor Southwick <twsouthwick@outlook.com>
2021-06-11 16:39:08 -0700

    Refactor observable stuff

---
+++ src/MyCentral.Components/MyCentralObservableExtensions.cs
@@ -0,0 +1,31 @@
+﻿using MyCentral.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace MyCentral.Components
+{
+    public static class MyCentralObservableExtensions
+    {
+        public static IObservable<string> ObserveDeviceNames(this IServiceClient client)
+        {
+            return InitialList().ToObservable()
+                .Merge(client.Events.Select(e => e.DeviceId))
+                .Distinct();
+
+            async IAsyncEnumerable<string> InitialList()
+            {
+                var devices = await client.GetDevicesAsync(default);
+
+                foreach (var device in devices.Devices)
+                {
+                    yield return device.Name;
+                }
+            }
+        }
+
+        public static IObservable<Event> ObserveEventsFor(this IServiceClient client, string deviceId)
+            => client.Events.Where(e => string.Equals(e.DeviceId, deviceId, StringComparison.Ordinal));
+    }
+}
---
+++ src/MyCentral.Components/ObservableList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading;
+
+namespace MyCentral.Components
+{
+    public class ObservableList<T> : IReadOnlyCollection<T>, IObserver<T>, IDisposable
+    {
+        private readonly IDisposable _disposable;
+        private readonly Action? _onChange;
+        private ImmutableList<T> _list = ImmutableList<T>.Empty;
+
+        public ObservableList(IObservable<T> observer, Action? onChange = null)
+        {
+            _disposable = observer.Subscribe(this);
+            _onChange = onChange;
+        }
+
+        public int Count => _list.Count;
+
+        public void Dispose()
+        {
+            _disposable.Dispose();
+        }
+
+        public void Clear()
+        {
+            Interlocked.Exchange(ref _list, ImmutableList<T>.Empty);
+            _onChange?.Invoke();
+        }
+
+        public IEnumerator<T> GetEnumerator() => _list.GetEnumerator();
+
+        public void OnCompleted()
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+        }
+
+        public void OnNext(T value)
+        {
+            Interlocked.Exchange(ref _list, _list.Insert(0, value));
+            _onChange?.Invoke();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
