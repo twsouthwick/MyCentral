b0387187f88d6aa4195dc475e1b37d0e4fdf4ebd
Taylor Southwick <tasou@microsoft.com>
2021-06-18 11:24:00 -0700

    Clean up emulated device

--- src/MyCentral.Device.Emulation/EmulatedDeviceFactory.cs
+++ src/MyCentral.Device.Emulation/EmulatedDeviceFactory.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Azure.Devices.Client;
 using MyCentral.Client;
-using System;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,50 +20,36 @@ namespace MyCentral.Device.Emulation
             var dc = DeviceClient.CreateFromConnectionString(device.GetConnectionString(_client.HostName), TransportType.Mqtt, new ClientOptions { ModelId = device.ModelId });
             var client = new PnPClient(dc);
 
-            await client.ReportComponentPropertyAsync("deviceInfo", "manufacturer", "rido");
-            var manufacturer = await client.ReadReportedComponentPropertyAsync<string>("deviceInfo", "manufacturer");
-
-            //await client.SendAsync("themorstat1", new { temperature = 11 });
-            //await client.SendAsync("themorstat2", new { temperature = 22 });
+            await client.ReportComponentPropertyAsync("deviceInfo", "manufacturer", "mycentral");
 
             client.SetDesiredPropertyUpdateCommandHandler("thermostat1", async (twin) =>
             {
-                Console.WriteLine("T1 " + twin.ToJson());
                 var targetTemp1 = twin.GetPropertyValue<int>("thermostat1", "targetTemperature");
                 await client.AckDesiredPropertyReadAsync("thermostat1", "targetTemperature", targetTemp1, StatusCodes.Completed, "tt1 received", twin.Version);
-                Console.WriteLine(targetTemp1);
             });
 
             client.SetDesiredPropertyUpdateCommandHandler("thermostat2", async (twin) =>
             {
-                Console.WriteLine("T2 " + twin.ToJson());
                 var targetTemp2 = twin.GetPropertyValue<int>("thermostat2", "targetTemperature");
                 await client.AckDesiredPropertyReadAsync("thermostat2", "targetTemperature", targetTemp2, StatusCodes.Completed, "tt2 received", twin.Version);
-                Console.WriteLine(targetTemp2);
             });
 
             await client.SetCommandHandlerAsync("reboot", async (req, ctx) =>
             {
-                Console.WriteLine(req.Name);
-                Console.WriteLine(req.DataAsJson);
                 await Task.Delay(1);
-                return new MethodResponse(UTF8Encoding.UTF8.GetBytes("{}"), 200);
+                return new MethodResponse(Encoding.UTF8.GetBytes("{}"), 200);
             }, null);
 
             await client.SetComponentCommandHandlerAsync("thermostat1", "getMaxMinReport", async (req, ctx) =>
             {
-                Console.WriteLine(req.Name);
-                Console.WriteLine(req.DataAsJson);
                 await Task.Delay(1);
-                return new MethodResponse(UTF8Encoding.UTF8.GetBytes("{}"), 200);
+                return new MethodResponse(Encoding.UTF8.GetBytes("{}"), 200);
             }, null);
 
             await client.SetComponentCommandHandlerAsync("thermostat2", "getMaxMinReport", async (req, ctx) =>
             {
-                Console.WriteLine(req.Name);
-                Console.WriteLine(req.DataAsJson);
                 await Task.Delay(1);
-                return new MethodResponse(UTF8Encoding.UTF8.GetBytes("{}"), 200);
+                return new MethodResponse(Encoding.UTF8.GetBytes("{}"), 200);
             }, null);
 
             return client;
